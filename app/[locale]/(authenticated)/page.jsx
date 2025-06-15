9f1c4acc6d237c04c31b056f71ec042bf1d2a2ed
Sikandar Chishty <sikandar.chishty@gmail.com>
2025-06-15 13:14:31 +0500

    v.1.9.0 release

--- app/[locale]/(authenticated)/page.jsx
+++ app/[locale]/(authenticated)/page.jsx
@@ -1,25 +1,19 @@
 "use client";
 import PageContainer from "@/components/providers/PageContainer";
-import { FixStickyHeaderScrollError } from "@/lib/utils";
+import { cn, FixStickyHeaderScrollError } from "@/lib/utils";
 import { useEffect, useRef, useState } from "react";
 import SingleItem from "@/components/items/SingleItem";
 import Summary from "@/components/items/Summary";
-import { Plus, Search } from "lucide-react";
-import { Input } from "@/components/ui/input";
 import { useData } from "@/components/contexts/DataContext";
 import MonthSwitcher from "@/components/items/MonthSwitcher";
-import { AddButtonWrapper } from "@/components/theme/AddButtonWrapper";
-import { Button } from "@/components/ui/button";
-import AddItemDialog from "@/components/items/AddItemDialog";
-import Navbar from "@/components/nav/Navbar";
 import ScrollToTopButton from "@/components/ScrollToTopButton";
+import { Button } from "@/components/ui/button";
 
 export default function Page() {
 	const scrollRef = useRef(null);
-
-	const [openAddPaymentDialog, setOpenAddPaymentDialog] = useState(false)
-	const [searchTerm, setSearchTerm] = useState("")
+	// const [searchTerm, setSearchTerm] = useState("")
 	const { items } = useData()
+	const [filterBy, setFilterBy] = useState("payment")
 
 	useEffect(() => {
 		if (scrollRef.current) {
@@ -45,11 +39,30 @@ export default function Page() {
 					/>
 				</div> */}
 
+			<div className="flex p-1 gap-1 bg-muted rounded-lg self-start ml-auto">
+				<Button
+					size="sm"
+					variant="secondary"
+					onClick={() => setFilterBy("payment")}
+					className={cn("min-h-7 bg-transparent hover:bg-background font-semibold text-[13px]", filterBy === "payment" ? "bg-background shadow" : "opacity-70")}            >
+					Payments
+				</Button>
+				<Button
+					size="sm"
+					variant="secondary"
+					onClick={() => setFilterBy("income")}
+					className={cn("min-h-7 bg-transparent hover:bg-background font-semibold text-[13px]", filterBy === "income" ? "bg-background shadow" : "opacity-70")}
+				>
+					Incomes
+				</Button>
+			</div>
+
 			<div className="gap-6 flex flex-col">
 				{items.length > 0 ?
-					items.map((item) => (
-						<SingleItem item={item} key={item.$id} />
-					))
+					items.filter((item) => item.type === filterBy)
+						.map((item) => (
+							<SingleItem item={item} key={item.$id} />
+						))
 					:
 					<div className="text-center italic text-muted-foreground mt-4">No Items Found for this month</div>
 				}
